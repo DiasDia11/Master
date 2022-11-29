40fe595ba87b42ce20541406eaae1f8db10bf75f
DiasDia11 <106533792+DiasDia11@users.noreply.github.com>
2022-11-29 14:00:20 +0000

    Estudo de css no React

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ManageData from './Components/ManageData';
 import ListRender from './Components/ListRender';
 import ConditionalRender from './Components/ConditionalRender';
 import ShowUserName from './Components/ShowUserName';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import CarDetails from './Components/CarDetails';
 import Fragments from './Components/Fragments';
 import Container from './Components/Container';
@@ -14,11 +14,14 @@ import ChangeMessageState from './Components/ChangeMessageState';
 // styles / css
 import './App.css';
 import Octocat from './assets/Octocat.png';
+import MyComponent from './ComponentsCSS/MyComponent';
+import Title from './ComponentsCSS/Title';
 
 
 
 
 function App() {
+  {/* 
   const name = "Eduardo";
   const [userName] = useState("Maria");
 
@@ -37,10 +40,122 @@ function App() {
   const handleMessage = (msg) => {
     setMessage(msg);
   }
+  */}
+
+  const n = 15;
+  const [name] = useState("Matheus");
+
+  const redTitle = true;
 
   return (
     <div className="App">
-      <h1>Avançando no React!</h1>
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      {/* CSS global
+      <h1>React com CSS</h1>
+       */}
+      {/* CSS de Componente 
+      <MyComponent />
+      <p>Este parágrafo é do app.js</p>
+      */}
+      {/* Inline CSS 
+      <p style={{
+        color: "magenta", 
+        padding: "25px", 
+        borderTop: "2px solid red"}}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      */}
+      {/* Inline Style Dinâmico 
+      <h2 style={n < 10 ? ({color: "purple"}) : ({color: "pink"})}>
+        Css Dinâmico
+      </h2>
+      <h2 style={n > 10 ? ({color: "purple"}) : ({color: "pink"})}>
+        Css Dinâmico
+      </h2>
+      <h2 style={
+        name === "Matheus" 
+        ? {color: "green", backgroundColor: "#000"}
+        : null
+      }
+      >
+        Teste nome
+      </h2>
+      */}
+      {/* Classe Dinâmica 
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este título vai ter classe Dinâmica
+      </h2>
+      */}
+      {/* CSS Modules 
+      <Title />
+      */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       {/* Imagem em asset 
       <div>
         <img src={Octocat} alt="gato" />
@@ -50,10 +165,12 @@ function App() {
       <ListRender />
       <ConditionalRender/>
       */}
-      {/* props */}
+      {/* props 
       <ShowUserName name={userName} />
-      {/* destructuring */}
+      */}
+      {/* destructuring 
       <CarDetails brand="VW" km={1000} color="Azul" newCar={false}/>
+      */}
       {/* reaproveitamento 
       <CarDetails brand="Ford" km={0} color="Vermelho" newCar={true}/>
       <CarDetails brand="Reanult" km={4500} color="Cinza" newCar={false}/>
@@ -68,20 +185,24 @@ function App() {
         />
       ))}
       */}
-      {/* fragment */}
+      {/* fragment 
       <Fragments propFragment="teste"/>
-      {/* children */}
+      */}
+      {/* children 
       <Container myValue="testing">
         <p>E este é o conteúdo</p>
       </Container>  
       <Container myValue="testing 2">
         <h5>Testando o container</h5>
       </Container>
-      {/* executar função */}
+      */}
+      {/* executar função 
       <ExecuteFunction myFunction={ShowMessage} />
+      */}
       {/* state lift */}
-      <Message msg={message}/>
+      {/* <Message msg={message}/>
       <ChangeMessageState handleMessage={handleMessage}/>
+      */}
     </div>
   );
 }
